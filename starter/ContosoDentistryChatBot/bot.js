3f0583620c121440c97104b504f4f543c9b7cb23
AllergictoCrustaceans <katie.wy.wu@gmail.com>
2022-02-23 20:49:57 -0800

    Add gitignore file to exclude .env and node_modules, install dependencies, and fill in code.

--- starter/ContosoDentistryChatBot/bot.js
+++ starter/ContosoDentistryChatBot/bot.js
@@ -5,7 +5,7 @@ const { ActivityHandler, MessageFactory } = require('botbuilder');
 
 const { QnAMaker } = require('botbuilder-ai');
 const DentistScheduler = require('./dentistscheduler');
-const IntentRecognizer = require("./intentrecognizer")
+const IntentRecognizer = require('./intentrecognizer');
 
 class DentaBot extends ActivityHandler {
     constructor(configuration, qnaOptions) {
@@ -14,45 +14,62 @@ class DentaBot extends ActivityHandler {
         if (!configuration) throw new Error('[QnaMakerBot]: Missing parameter. configuration is required');
 
         // create a QnAMaker connector
-        this.QnAMaker = new QnAMaker()
-       
-        // create a DentistScheduler connector
-      
-        // create a IntentRecognizer connector
+        this.QnAMaker = new QnAMaker(configuration.QnAConfiguration, qnaOptions);
 
+        // create a DentistScheduler connector
+        this.DentistScheduler = new DentistScheduler(configuration.SchedulerConfiguration);
+
+        // create a IntentRecognizer connector
+        this.IntentRecognizer = new IntentRecognizer(configuration.LuisConfiguration);
 
         this.onMessage(async (context, next) => {
             // send user input to QnA Maker and collect the response in a variable
             // don't forget to use the 'await' keyword
-          
+            const qnaResults = await this.QnAMaker.getAnswers(context);
+
             // send user input to IntentRecognizer and collect the response in a variable
             // don't forget 'await'
-                     
-            // determine which service to respond with based on the results from LUIS //
+            const LuisResult = await this.IntentRecognizer.executeLuisQuery(context);
 
-            // if(top intent is intentA and confidence greater than 50){
-            //  doSomething();
-            //  await context.sendActivity();
-            //  await next();
-            //  return;
-            // }
-            // else {...}
-             
+            // determine which service to respond with based on the results from LUIS //
+            if (LuisResult.LuisResult.prediction.topIntent === 'scheduleAppointment' &&
+                LuisResult.intents.scheduleAppointment.score > 0.6 &&
+                LuisResult.entities.$instance &&
+                LuisResult.entities.$instance.time &&
+                LuisResult.entities.$instance.time[0]
+            ) {
+                const time = LuisResult.entities.$instance.time[0].text;
+                const day = LuisResult.entities.$instance.day[0].text;
+                const date = LuisResult.entities.$instance.date[0].text;
+                const getTimeOfAvailability = 'Great! We have an opening on ' + date ? date : day + 'at ' + time + '. See you then!';
+                console.log(getTimeOfAvailability);
+                await context.sendActivity(getTimeOfAvailability);
+                await next();
+                return;
+            }
+
+            if (qnaResults[0]) {
+                console.log(qnaResults[0]);
+                await context.sendActivity(`${ qnaResults[0].answer }`);
+            } else {
+                await context.sendActivity('Im not sure I found an answer to your question. Sorry :(');
+            }
+
             await next();
-    });
+        });
 
         this.onMembersAdded(async (context, next) => {
-        const membersAdded = context.activity.membersAdded;
-        //write a custom greeting
-        const welcomeText = '';
-        for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-            if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+            const membersAdded = context.activity.membersAdded;
+            // write a custom greeting
+            const welcomeText = '';
+            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+                if (membersAdded[cnt].id !== context.activity.recipient.id) {
+                    await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+                }
             }
-        }
-        // by calling next() you ensure that the next BotHandler is run.
-        await next();
-    });
+            // by calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
     }
 }
 
--- starter/ContosoDentistryChatBot/dentistscheduler.js
+++ starter/ContosoDentistryChatBot/dentistscheduler.js
@@ -1,22 +1,24 @@
+import fetch from 'cross-fetch';
+
 class DentistScheduler {
     constructor(configuration) {
         this.getAvailability = async () => {
-            const response = await fetch(configuration.SchedulerEndpoint + "availability")
-            const times = await response.json()
-            let responseText = `Current time slots available: `
+            const response = await fetch(configuration.SchedulerEndpoint + 'availability');
+            const times = await response.json();
+            let responseText = 'Current time slots available: ';
             times.map(time => {
-                responseText += `
-${time}`
-            })
-            return responseText
-        }
+                responseText += `${ time }`;
+            });
+            return responseText;
+        };
 
         this.scheduleAppointment = async (time) => {
-            const response = await fetch(configuration.SchedulerEndpoint + "schedule", { method: "post", body: { time: time } })
-            let responseText = `An appointment is set for ${time}.`
-            return responseText
-        }
+            const response = await fetch(configuration.SchedulerEndpoint + 'schedule', { method: 'post', body: { time: time } });
+            const responseTime = response.json();
+            const responseText = `An appointment is set for ${ responseTime }.`;
+            return responseText;
+        };
     }
 }
 
-module.exports = DentistScheduler
+module.exports = DentistScheduler;

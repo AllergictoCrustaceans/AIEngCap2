37581bfd5d4104a182dbeb1e64c2fe70b1706634
AllergictoCrustaceans <katie.wy.wu@gmail.com>
2022-03-01 13:30:24 -0800

    try to configure deployment path again.

--- starter/ContosoDentistryChatBot/bot.js
+++ starter/ContosoDentistryChatBot/bot.js
@@ -27,27 +27,6 @@ class DentaBot extends ActivityHandler {
             // don't forget to use the 'await' keyword
             const qnaResults = await this.QnAMaker.getAnswers(context);
 
-            // send user input to IntentRecognizer and collect the response in a variable
-            // don't forget 'await'
-            const LuisResult = await this.IntentRecognizer.executeLuisQuery(context);
-
-            // determine which service to respond with based on the results from LUIS //
-            if (LuisResult.LuisResult.prediction.topIntent === 'scheduleAppointment' &&
-                LuisResult.intents.scheduleAppointment.score > 0.6 &&
-                LuisResult.entities.$instance &&
-                LuisResult.entities.$instance.time &&
-                LuisResult.entities.$instance.time[0]
-            ) {
-                const time = LuisResult.entities.$instance.time[0].text;
-                const day = LuisResult.entities.$instance.day[0].text;
-                const date = LuisResult.entities.$instance.date[0].text;
-                const getTimeOfAvailability = 'Great! We have an opening on ' + date ? date : day + 'at ' + time + '. See you then!';
-                console.log(getTimeOfAvailability);
-                await context.sendActivity(getTimeOfAvailability);
-                await next();
-                return;
-            }
-
             if (qnaResults[0]) {
                 console.log(qnaResults[0]);
                 await context.sendActivity(`${ qnaResults[0].answer }`);
@@ -55,13 +34,35 @@ class DentaBot extends ActivityHandler {
                 await context.sendActivity('Im not sure I found an answer to your question. Sorry :(');
             }
 
+            // send user input to IntentRecognizer and collect the response in a variable
+            // don't forget 'await'
+            const intentResults = await this.IntentRecognizer.executeLuisQuery(context);
+            console.log('What is the luis result: ', intentResults);
+
+            // determine which service to respond with based on the results from LUIS
+            // if (LuisResult.LuisResult.prediction.topIntent === 'getAvailability' &&
+            //     LuisResult.intents.scheduleAppointment.score > 0.6 &&
+            //     LuisResult.entities.$instance &&
+            //     LuisResult.entities.$instance.time &&
+            //     LuisResult.entities.$instance.time[0]
+            // ) {
+            //     const time = LuisResult.entities.$instance.time[0].text;
+            //     const day = LuisResult.entities.$instance.day[0].text;
+            //     const date = LuisResult.entities.$instance.date[0].text;
+            //     const getTimeOfAvailability = 'Great! We have an opening on ' + date ? date : day + 'at ' + time + '. See you then!';
+            //     console.log(getTimeOfAvailability);
+            //     await context.sendActivity(getTimeOfAvailability);
+            //     await next();
+            //     return;
+            // }
+
             await next();
         });
 
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
             // write a custom greeting
-            const welcomeText = 'Heyo! Welcome to Spongebob Dental Clinic. Please ask us any questions, or schedule an appointment with us :D';
+            const welcomeText = 'Heyo! Welcome to Contoso Dental Clinic. Please ask us any questions, or schedule an appointment with us :D';
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
                     await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
